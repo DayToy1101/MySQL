b87a6698dea9ee7b69941ed443d344f03f49d7c3
a <2573263498@qq.com>
2020-01-04 23:37:36 +0800

    20200104

---
+++ tb_authorinfo.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tb_authorinfo]
+(
+	[authorId]       VARCHAR (50)  NOT NULL primary key,
+    [authorName]     VARCHAR (50)  NOT NULL,
+    [authorSex]      VARCHAR (50)  NOT NULL,
+    [authorbirthday] DATETIME2 (7) NULL,
+    [authorGenre]    VARCHAR (50)  NULL,
+    [authorcompany]  VARCHAR (100) NULL,
+    [authorRecma]    VARCHAR (50)  NULL,
+    [authorzjm]      VARCHAR (50)  NULL,
+    [RdateTime]      TIME (7)      NULL,
+    PRIMARY KEY CLUSTERED ([authorId] ASC)
+);
