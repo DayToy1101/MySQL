e2f900eec8b7d6df14a76a4b821716b52c7ea2c5
a <2573263498@qq.com>
2020-01-04 23:38:08 +0800

    20200104

---
+++ tb_computer.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[tb_computer]
+(
+	 [cmp_ID]   VARCHAR (20) NOT NULL primary key,
+    [cmp_name] VARCHAR (30) NULL,
+    [cmp_Paww] VARCHAR (50) NULL,
+    [cmp_Falg] INT          NULL,
+    PRIMARY KEY CLUSTERED ([cmp_ID] ASC)
+)
