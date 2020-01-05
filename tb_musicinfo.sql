ef1cedd76b47be9eed2b8f2105edf29415e356e9
a <2573263498@qq.com>
2020-01-05 21:57:03 +0800

    20200105

---
+++ tb_musicinfo.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tb_musicinfo]
+(
+	 [Music_code]     VARCHAR (50) NOT NULL,
+    [Music_name]     VARCHAR (50) NOT NULL,
+    [Music_author]   VARCHAR (50) NULL,
+    [Music_Kind]     VARCHAR (50) NULL,
+    [Music_chinse]   VARCHAR (50) NULL,
+    [Music_Ping]     VARCHAR (50) NULL,
+    [Music_falg]     INT          NULL,
+    [Music_date]     TIME (7)     NULL,
+    [Music_filepath] VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Music_code] ASC)
+)
